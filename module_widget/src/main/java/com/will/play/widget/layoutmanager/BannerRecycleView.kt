f62c39cc05912c14769cb83c82b2879d4f6cebb1
will <545512533@qq.com>
2020-06-24 00:59:30 +0800

    添加支付模块

---
+++ module_widget/src/main/java/com/will/play/widget/layoutmanager/BannerRecycleView.kt
@@ -0,0 +1,7 @@
+package com.will.play.widget.layoutmanager
+
+import android.content.Context
+import androidx.recyclerview.widget.RecyclerView
+
+class BannerRecycleView(context: Context) :RecyclerView(context) {
+}
