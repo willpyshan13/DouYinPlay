b17630ed98e7e933586ebe4b879382119e06245b
will <545512533@qq.com>
2020-06-27 00:38:22 +0800

    添加首页item布局

--- play_home/src/main/java/com/will/play/home/ui/viewmodel/FragmentHomeViewModel.kt
+++ play_home/src/main/java/com/will/play/home/ui/viewmodel/FragmentHomeViewModel.kt
@@ -1,11 +1,19 @@
 package com.will.play.home.ui.viewmodel
 
 import android.app.Application
-import com.will.habit.base.BaseViewModel
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.will.habit.base.BaseListViewModel
+import com.will.habit.base.ItemViewModel
+import com.will.habit.extection.launch
+import com.will.habit.widget.recycleview.paging.LoadCallback
+import com.will.play.home.R
+import com.will.play.home.BR
 import com.will.play.home.repository.HomeRepository
+import me.tatarka.bindingcollectionadapter2.ItemBinding
 
 /**
- * Desc:
+ * Desc:首页
  *
  * Date: 2020-06-22
  * Copyright: Copyright (c) 2018-2020
@@ -15,5 +23,48 @@ import com.will.play.home.repository.HomeRepository
  *
  * @Author: pengyushan
  */
-class FragmentHomeViewModel(application: Application) :BaseViewModel<HomeRepository>(application) {
+class FragmentHomeViewModel(application: Application) :BaseListViewModel<HomeRepository, ItemViewModel<*>>(application) {
+    override fun getDiffItemCallback(): DiffUtil.ItemCallback<ItemViewModel<*>> {
+        return object : DiffUtil.ItemCallback<ItemViewModel<*>>() {
+            override fun areItemsTheSame(oldItem: ItemViewModel<*>, newItem: ItemViewModel<*>): Boolean {
+                return false
+            }
+
+            override fun areContentsTheSame(oldItem: ItemViewModel<*>, newItem: ItemViewModel<*>): Boolean {
+                return false
+            }
+
+        }
+    }
+
+    init {
+        loadInit()
+    }
+
+    override fun showEmptyState() {
+    }
+
+    override fun getItemBinding(): ItemBinding<ItemViewModel<*>> {
+        return ItemBinding.of { binding, _, item ->
+            when(item){
+                is HomeDataItem -> binding.set(BR.viewModel, R.layout.fragment_home_item)
+                is HomeHeaderItem -> binding.set(BR.viewModel, R.layout.fragment_home_header)
+            }
+        }
+    }
+
+    override fun getItemDecoration(): RecyclerView.ItemDecoration? {
+        return null
+    }
+
+    override fun loadData(pageIndex: Int, loadCallback: LoadCallback<ItemViewModel<*>>) {
+        launch({
+            val viewModels = mutableListOf<ItemViewModel<*>>()
+            viewModels.add(HomeHeaderItem(this))
+            viewModels.add(HomeDataItem(this))
+            items.addAll(viewModels)
+        },{
+
+        })
+    }
 }
---
+++ play_home/src/main/java/com/will/play/home/ui/viewmodel/HomeHeaderItem.kt
@@ -0,0 +1,6 @@
+package com.will.play.home.ui.viewmodel
+
+import com.will.habit.base.ItemViewModel
+
+class HomeHeaderItem(viewModel:FragmentHomeViewModel) :ItemViewModel<FragmentHomeViewModel>(viewModel) {
+}
