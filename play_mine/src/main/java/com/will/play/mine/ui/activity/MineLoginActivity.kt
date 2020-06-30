1c77085fe5582b4597d17e4fce5afbdc9a9ddfa0
will <545512533@qq.com>
2020-07-01 00:01:01 +0800

    添加登陆页面资源文件

---
+++ play_mine/src/main/java/com/will/play/mine/ui/activity/MineLoginActivity.kt
@@ -0,0 +1,28 @@
+package com.will.play.mine.ui.activity
+
+import android.os.Bundle
+import com.will.habit.base.BaseActivity
+import com.will.play.mine.BR
+import com.will.play.mine.R
+import com.will.play.mine.databinding.MineActivityAddressBinding
+import com.will.play.mine.ui.viewmodel.MineAddressViewModel
+import com.will.play.mine.ui.viewmodel.MineLoginViewModel
+
+/**
+ * Desc:登陆
+ *
+ * Date: 2020-06-30
+ * Update Time:
+ * Update Comments:
+ *
+ * @Author: pengyushan
+ */
+class MineLoginActivity : BaseActivity<MineActivityAddressBinding, MineLoginViewModel>() {
+    override fun initContentView(savedInstanceState: Bundle?): Int {
+        return R.layout.mine_activity_login
+    }
+
+    override fun initVariableId(): Int {
+        return BR.viewModel
+    }
+}
