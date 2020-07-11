28fde25a319e196a094908819f46133a277afa7e
will <545512533@qq.com>
2020-07-11 17:24:30 +0800

    1、修改声音设置页面

--- business/play_mine/src/main/java/com/will/play/mine/ui/viewmodel/MineSettingViewModel.kt
+++ business/play_mine/src/main/java/com/will/play/mine/ui/viewmodel/MineSettingViewModel.kt
@@ -6,12 +6,15 @@ import androidx.recyclerview.widget.RecyclerView
 import com.will.habit.base.BaseListViewModel
 import com.will.habit.base.BaseViewModel
 import com.will.habit.base.ItemViewModel
+import com.will.habit.binding.command.BindingAction
+import com.will.habit.binding.command.BindingCommand
 import com.will.habit.extection.launch
 import com.will.habit.utils.StringUtils
 import com.will.habit.widget.recycleview.paging.LoadCallback
 import com.will.play.mine.R
 import com.will.play.mine.BR
 import com.will.play.mine.repository.MineRepository
+import com.will.play.mine.ui.activity.MineSettingVolumeActivity
 import me.tatarka.bindingcollectionadapter2.ItemBinding
 
 /**
@@ -31,4 +34,16 @@ class MineSettingViewModel(application: Application) :BaseViewModel<MineReposito
         super.onCreate()
         setTitleText(StringUtils.getStringResource(R.string.mine_setting_title))
     }
+
+    val onVolumeClick = BindingCommand<Any>(object : BindingAction {
+        override fun call() {
+            startActivity(MineSettingVolumeActivity::class.java)
+        }
+    })
+
+    val onCheckUpdateClick = BindingCommand<Any>(object : BindingAction {
+        override fun call() {
+
+        }
+    })
 }
--- business/play_mine/src/main/java/com/will/play/mine/ui/viewmodel/MineSettingVolumeViewModel.kt
+++ business/play_mine/src/main/java/com/will/play/mine/ui/viewmodel/MineSettingVolumeViewModel.kt
@@ -7,6 +7,7 @@ import com.will.habit.base.BaseListViewModel
 import com.will.habit.base.BaseViewModel
 import com.will.habit.base.ItemViewModel
 import com.will.habit.extection.launch
+import com.will.habit.utils.StringUtils
 import com.will.habit.widget.recycleview.paging.LoadCallback
 import com.will.play.mine.R
 import com.will.play.mine.BR
@@ -14,7 +15,7 @@ import com.will.play.mine.repository.MineRepository
 import me.tatarka.bindingcollectionadapter2.ItemBinding
 
 /**
- * Desc:我的地址页面
+ * Desc:设置音量
  *
  * Date: 2020-06-22
  * Copyright: Copyright (c) 2018-2020
@@ -26,4 +27,8 @@ import me.tatarka.bindingcollectionadapter2.ItemBinding
  */
 class MineSettingVolumeViewModel(application: Application) :BaseViewModel<MineRepository>(application) {
 
+    override fun onCreate() {
+        super.onCreate()
+        setTitleText(StringUtils.getStringResource(R.string.mine_volume_setting_title))
+    }
 }
