f57fff0d45cc649531cb21f0469511f4783f0bc3
will <545512533@qq.com>
2020-07-11 15:27:59 +0800

    1、个人中心item跳转事件添加

--- business/play_mine/src/main/java/com/will/play/mine/ui/viewmodel/MineDataItem.kt
+++ business/play_mine/src/main/java/com/will/play/mine/ui/viewmodel/MineDataItem.kt
@@ -1,6 +1,49 @@
 package com.will.play.mine.ui.viewmodel
 
+import android.graphics.drawable.Drawable
+import androidx.core.content.ContextCompat
+import androidx.databinding.ObservableField
 import com.will.habit.base.ItemViewModel
+import com.will.habit.binding.command.BindingAction
+import com.will.habit.binding.command.BindingCommand
+import com.will.habit.utils.Utils
+import com.will.play.mine.ui.activity.MineDouyinBindingActivity
+import com.will.play.mine.ui.activity.MineWalletActivity
 
-class MineDataItem(viewModel:MineViewModel,val resIcon:Int,val title:String) :ItemViewModel<MineViewModel>(viewModel) {
+/**
+ * 设置中心，item
+ */
+class MineDataItem(viewModel: MineViewModel, private val resIcon: Int, val title: String, itemType: Int) : ItemViewModel<MineViewModel>(viewModel) {
+    val imageDrawable = ObservableField<Drawable>()
+
+    init {
+        imageDrawable.set(ContextCompat.getDrawable(Utils.getContext(), resIcon))
+    }
+
+    val itemClick = BindingCommand<Any>(object : BindingAction {
+        override fun call() {
+            when (itemType) {
+                item_type_wallet -> viewModel.startActivity(MineWalletActivity::class.java)
+                item_type_message -> viewModel.startActivity(MineWalletActivity::class.java)
+                item_type_binding_taobao -> viewModel.startActivity(MineWalletActivity::class.java)
+                item_type_binding_douyin -> viewModel.startActivity(MineDouyinBindingActivity::class.java)
+                item_type_course -> viewModel.startActivity(MineWalletActivity::class.java)
+                item_type_guide -> viewModel.startActivity(MineWalletActivity::class.java)
+                item_type_contract -> viewModel.startActivity(MineWalletActivity::class.java)
+                else -> viewModel.startActivity(MineWalletActivity::class.java)
+
+            }
+        }
+    })
+
+    companion object {
+        const val item_type_wallet = 0
+        const val item_type_message = 1
+        const val item_type_binding_taobao = 2
+        const val item_type_binding_douyin = 3
+        const val item_type_course = 4
+        const val item_type_guide = 5
+        const val item_type_contract = 6
+        const val item_type_custom_service = 7
+    }
 }
--- business/play_mine/src/main/java/com/will/play/mine/ui/viewmodel/MineViewModel.kt
+++ business/play_mine/src/main/java/com/will/play/mine/ui/viewmodel/MineViewModel.kt
@@ -5,11 +5,21 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.will.habit.base.BaseListViewModel
 import com.will.habit.base.ItemViewModel
+import com.will.habit.binding.command.BindingAction
+import com.will.habit.binding.command.BindingCommand
 import com.will.habit.extection.launch
 import com.will.habit.widget.recycleview.paging.LoadCallback
 import com.will.play.mine.R
 import com.will.play.mine.BR
 import com.will.play.mine.repository.MineRepository
+import com.will.play.mine.ui.viewmodel.MineDataItem.Companion.item_type_binding_douyin
+import com.will.play.mine.ui.viewmodel.MineDataItem.Companion.item_type_binding_taobao
+import com.will.play.mine.ui.viewmodel.MineDataItem.Companion.item_type_contract
+import com.will.play.mine.ui.viewmodel.MineDataItem.Companion.item_type_course
+import com.will.play.mine.ui.viewmodel.MineDataItem.Companion.item_type_custom_service
+import com.will.play.mine.ui.viewmodel.MineDataItem.Companion.item_type_guide
+import com.will.play.mine.ui.viewmodel.MineDataItem.Companion.item_type_message
+import com.will.play.mine.ui.viewmodel.MineDataItem.Companion.item_type_wallet
 import me.tatarka.bindingcollectionadapter2.ItemBinding
 
 /**
@@ -23,7 +33,7 @@ import me.tatarka.bindingcollectionadapter2.ItemBinding
  *
  * @Author: pengyushan
  */
-class MineViewModel(application: Application) :BaseListViewModel<MineRepository, ItemViewModel<*>>(application) {
+class MineViewModel(application: Application) : BaseListViewModel<MineRepository, ItemViewModel<*>>(application) {
     override fun getDiffItemCallback(): DiffUtil.ItemCallback<ItemViewModel<*>> {
         return object : DiffUtil.ItemCallback<ItemViewModel<*>>() {
             override fun areItemsTheSame(oldItem: ItemViewModel<*>, newItem: ItemViewModel<*>): Boolean {
@@ -41,12 +51,26 @@ class MineViewModel(application: Application) :BaseListViewModel<MineRepository,
         loadInit()
     }
 
+    val leftClick = BindingCommand<Any>(object : BindingAction {
+        override fun call() {
+
+        }
+
+    })
+
+    val rightCLick = BindingCommand<Any>(object : BindingAction {
+        override fun call() {
+
+        }
+
+    })
+
     override fun showEmptyState() {
     }
 
     override fun getItemBinding(): ItemBinding<ItemViewModel<*>> {
         return ItemBinding.of { binding, _, item ->
-            when(item){
+            when (item) {
                 is MineDataItem -> binding.set(BR.viewModel, R.layout.fragment_mine_item)
                 is MineHeaderItem -> binding.set(BR.viewModel, R.layout.fragment_mine_header)
             }
@@ -61,16 +85,16 @@ class MineViewModel(application: Application) :BaseListViewModel<MineRepository,
         launch({
             val viewModels = mutableListOf<ItemViewModel<*>>()
             viewModels.add(MineHeaderItem(this))
-            viewModels.add(MineDataItem(this,R.mipmap.base_mine_wallet,"我的钱包"))
-            viewModels.add(MineDataItem(this,R.mipmap.base_mine_course,"我的消息"))
-            viewModels.add(MineDataItem(this,R.mipmap.base_mine_taobao,"绑定淘宝"))
-            viewModels.add(MineDataItem(this,R.mipmap.base_mine_douyin,"绑定抖音"))
-            viewModels.add(MineDataItem(this,R.mipmap.base_mine_course,"我的课程"))
-            viewModels.add(MineDataItem(this,R.mipmap.base_mine_guide,"新手指引"))
-            viewModels.add(MineDataItem(this,R.mipmap.base_mine_contact,"联系我们"))
-            viewModels.add(MineDataItem(this,R.mipmap.base_mine_custom,"工人客服"))
+            viewModels.add(MineDataItem(this, R.mipmap.base_mine_wallet, "我的钱包", item_type_wallet))
+            viewModels.add(MineDataItem(this, R.mipmap.base_mine_course, "我的消息", item_type_message))
+            viewModels.add(MineDataItem(this, R.mipmap.base_mine_taobao, "绑定淘宝", item_type_binding_taobao))
+            viewModels.add(MineDataItem(this, R.mipmap.base_mine_douyin, "绑定抖音", item_type_binding_douyin))
+            viewModels.add(MineDataItem(this, R.mipmap.base_mine_course, "我的课程", item_type_course))
+            viewModels.add(MineDataItem(this, R.mipmap.base_mine_guide, "新手指引", item_type_guide))
+            viewModels.add(MineDataItem(this, R.mipmap.base_mine_contact, "联系我们", item_type_contract))
+            viewModels.add(MineDataItem(this, R.mipmap.base_mine_custom, "工人客服", item_type_custom_service))
             items.addAll(viewModels)
-        },{
+        }, {
 
         })
     }
