b78dbcd9e40c05ca097bc31ac45267b9a3b8cca5
will <545512533@qq.com>
2020-07-11 17:14:45 +0800

    1、修改设置页面

--- business/play_mine/src/main/java/com/will/play/mine/ui/viewmodel/MineSettingViewModel.kt
+++ business/play_mine/src/main/java/com/will/play/mine/ui/viewmodel/MineSettingViewModel.kt
@@ -7,6 +7,7 @@ import com.will.habit.base.BaseListViewModel
 import com.will.habit.base.BaseViewModel
 import com.will.habit.base.ItemViewModel
 import com.will.habit.extection.launch
+import com.will.habit.utils.StringUtils
 import com.will.habit.widget.recycleview.paging.LoadCallback
 import com.will.play.mine.R
 import com.will.play.mine.BR
@@ -26,4 +27,8 @@ import me.tatarka.bindingcollectionadapter2.ItemBinding
  */
 class MineSettingViewModel(application: Application) :BaseViewModel<MineRepository>(application) {
 
+    override fun onCreate() {
+        super.onCreate()
+        setTitleText(StringUtils.getStringResource(R.string.mine_setting_title))
+    }
 }
