c33a8ae378720db1d3a23d29e000bb19cab4c598
will <545512533@qq.com>
2020-07-11 18:10:03 +0800

    1、修改我的信息页面

--- business/play_mine/src/main/java/com/will/play/mine/ui/viewmodel/MineHomeHeaderItem.kt
+++ business/play_mine/src/main/java/com/will/play/mine/ui/viewmodel/MineHomeHeaderItem.kt
@@ -1,7 +1,18 @@
 package com.will.play.mine.ui.viewmodel
 
 import com.will.habit.base.ItemViewModel
+import com.will.habit.binding.command.BindingAction
+import com.will.habit.binding.command.BindingCommand
+import com.will.play.mine.ui.activity.MineInfoEditActivity
 
+/**
+ * @author will
+ */
 class MineHomeHeaderItem(viewModel:MineViewModel) :ItemViewModel<MineViewModel>(viewModel) {
 
+    val onMineInfoEditClick = BindingCommand<Any>(object : BindingAction {
+        override fun call() {
+            viewModel.startActivity(MineInfoEditActivity::class.java)
+        }
+    })
 }
