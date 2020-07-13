7db8ab4ed496d923d10c220815d010322eb8445e
will <545512533@qq.com>
2020-07-14 07:28:27 +0800

    1、添加我的会员详情跳转

--- business/play_mine/src/main/java/com/will/play/mine/ui/viewmodel/MineVipViewModel.kt
+++ business/play_mine/src/main/java/com/will/play/mine/ui/viewmodel/MineVipViewModel.kt
@@ -2,9 +2,12 @@ package com.will.play.mine.ui.viewmodel
 
 import android.app.Application
 import com.will.habit.base.BaseViewModel
+import com.will.habit.binding.command.BindingAction
+import com.will.habit.binding.command.BindingCommand
 import com.will.play.mine.R
 import com.will.play.mine.BR
 import com.will.play.mine.repository.MineRepository
+import com.will.play.mine.ui.activity.MineVipDetailActivity
 
 /**
  * Desc:我的地址页面
@@ -19,4 +22,11 @@ import com.will.play.mine.repository.MineRepository
  */
 class MineVipViewModel(application: Application) :BaseViewModel<MineRepository>(application) {
     val vipLayout = MineVipLayoutItem(this)
+
+    val onVipClick = BindingCommand<Any>(object :BindingAction{
+        override fun call() {
+            startActivity(MineVipDetailActivity::class.java)
+        }
+
+    })
 }
