8e65450bf0752945dc6c025040b9011a156ddc09
will <545512533@qq.com>
2020-07-15 06:30:01 +0800

    1、修改我的会员详情

--- business/play_mine/src/main/java/com/will/play/mine/ui/viewmodel/MineVipDetailViewModel.kt
+++ business/play_mine/src/main/java/com/will/play/mine/ui/viewmodel/MineVipDetailViewModel.kt
@@ -7,6 +7,7 @@ import com.will.habit.base.BaseListViewModel
 import com.will.habit.base.BaseViewModel
 import com.will.habit.base.ItemViewModel
 import com.will.habit.extection.launch
+import com.will.habit.utils.StringUtils
 import com.will.habit.widget.recycleview.paging.LoadCallback
 import com.will.play.mine.R
 import com.will.play.mine.BR
@@ -25,5 +26,7 @@ import me.tatarka.bindingcollectionadapter2.ItemBinding
  * @Author: pengyushan
  */
 class MineVipDetailViewModel(application: Application) :BaseViewModel<MineRepository>(application) {
-
+    init {
+        setTitleText(StringUtils.getStringResource(R.string.mine_vip_title))
+    }
 }
