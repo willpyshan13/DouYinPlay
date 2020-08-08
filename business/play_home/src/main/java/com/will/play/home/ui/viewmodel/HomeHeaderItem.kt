cffc0b89208c42f4a85a540b2ec12ea6c9c6bdb3
will <545512533@qq.com>
2020-08-09 01:22:00 +0800

    1、首页顶部数据填充

--- business/play_home/src/main/java/com/will/play/home/ui/viewmodel/HomeHeaderItem.kt
+++ business/play_home/src/main/java/com/will/play/home/ui/viewmodel/HomeHeaderItem.kt
@@ -1,6 +1,7 @@
 package com.will.play.home.ui.viewmodel
 
 import androidx.databinding.ObservableArrayList
+import androidx.databinding.ObservableField
 import com.will.habit.base.ItemViewModel
 import com.will.play.home.R
 import com.will.play.home.BR
@@ -8,6 +9,7 @@ import com.will.play.home.entity.HomeRespDataEntity
 import me.tatarka.bindingcollectionadapter2.ItemBinding
 
 class HomeHeaderItem(viewModel:FragmentHomeViewModel) :ItemViewModel<FragmentHomeViewModel>(viewModel) {
+    val dataObservable = ObservableField<HomeRespDataEntity>()
     /**
      * banner列表
      */
@@ -19,7 +21,7 @@ class HomeHeaderItem(viewModel:FragmentHomeViewModel) :ItemViewModel<FragmentHom
     }
 
     fun updateHeaderData(data: HomeRespDataEntity?){
-
+        dataObservable.set(data)
     }
 
 }
