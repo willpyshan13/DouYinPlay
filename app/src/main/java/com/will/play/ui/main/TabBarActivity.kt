ee9396bc8ac82e997d35d1ae940d8c98efeac3a4
pengyushan <pengyushan@vv.cn>
2020-08-13 17:08:06 +0800

    修改底部tab颜色

--- app/src/main/java/com/will/play/ui/main/TabBarActivity.kt
+++ app/src/main/java/com/will/play/ui/main/TabBarActivity.kt
@@ -62,10 +62,10 @@ class TabBarActivity : BaseActivity<ActivityTabBarBinding, TabBarViewModel>() {
 
     private fun initBottomTab() {
         val navigationController = binding.pagerBottomTab.material()
-                .addItem(R.mipmap.base_icon_home, R.mipmap.base_icon_home_select,"首页",resources.getColor(R.color.yellow))
-                .addItem(R.mipmap.base_icon_data,R.mipmap.base_icon_data_select, "数据",resources.getColor(R.color.yellow))
-                .addItem(R.mipmap.base_icon_pick, "选品",resources.getColor(R.color.yellow))
-                .addItem(R.mipmap.base_icon_mine, "我的",resources.getColor(R.color.yellow))
+                .addItem(R.mipmap.base_icon_home, R.mipmap.base_icon_home_select,"首页",resources.getColor(R.color.color_FDEC83))
+                .addItem(R.mipmap.base_icon_data,R.mipmap.base_icon_data_select, "数据",resources.getColor(R.color.color_FDEC83))
+                .addItem(R.mipmap.base_icon_pick, "选品",resources.getColor(R.color.color_FDEC83))
+                .addItem(R.mipmap.base_icon_mine, "我的",resources.getColor(R.color.color_FDEC83))
                 .build()
         //底部按钮的点击事件监听
         navigationController.addTabItemSelectedListener(object : OnTabItemSelectedListener {
