ee192e62672a32be9db619778fdd9e26155ef6a0
will <545512533@qq.com>
2020-08-26 10:27:16 +0800

    修改抖音授权的scope

--- base_module/module_third/src/main/java/com/will/play/third/DouyinLogin.kt
+++ base_module/module_third/src/main/java/com/will/play/third/DouyinLogin.kt
@@ -10,7 +10,7 @@ object DouyinLogin {
     fun login(activity: Activity){
         val douyinOpenApi = DouYinOpenApiFactory.create(activity);
         val request = Authorization.Request();
-        request.scope = "user_info"
+        request.scope = "user_info,aweme.share,video.data,video.list,video.create,video.delete,video.comment,item.comment,im,mobile,mobile_alert,video.search,data.external.item,data.external.user,fans.data,hotsearch"
         // 用户授权时必选权限
         //request.state = "ww";                                 // 用于保持请求和回调的状态，授权请求后原样带回给第三方。
         request.callerLocalEntry = "com.will.play.third.douyin.DouYinEntryActivity";
