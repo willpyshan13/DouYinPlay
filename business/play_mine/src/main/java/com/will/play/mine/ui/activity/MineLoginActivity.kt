9c9d3e746a9fe8daef4a5e8bb37c9bc1f3a2a92c
will <545512533@qq.com>
2020-09-27 21:12:26 +0800

    version1.1.12 添加登陆隐私条款

--- business/play_mine/src/main/java/com/will/play/mine/ui/activity/MineLoginActivity.kt
+++ business/play_mine/src/main/java/com/will/play/mine/ui/activity/MineLoginActivity.kt
@@ -3,12 +3,16 @@ package com.will.play.mine.ui.activity
 import android.os.Bundle
 import androidx.lifecycle.Observer
 import com.will.habit.base.BaseActivity
+import com.will.habit.utils.ClipboardUtils
+import com.will.habit.utils.ToastUtils
+import com.will.habit.widget.dialog.ConfirmDialog
 import com.will.play.mine.BR
 import com.will.play.mine.R
 import com.will.play.mine.databinding.MineActivityAddressBinding
 import com.will.play.mine.ui.viewmodel.MineLoginViewModel
 import com.will.play.third.DouyinLogin
 import com.will.play.third.ThirdInit
+import kotlinx.android.synthetic.main.mine_activity_login.*
 
 /**
  * Desc:登陆页面
@@ -40,6 +44,21 @@ class MineLoginActivity : BaseActivity<MineActivityAddressBinding, MineLoginView
         super.initViewObservable()
         viewModel.douyinLogin.observe(this, Observer {
             DouyinLogin.login(this)
+        })
+
+        viewModel.showPhoneCallDialog.observe(this, {
+            ConfirmDialog(this, true)
+                    .setTtitle("服务协议和隐私政策")
+                    .setTitleSize(15F)
+                    .setMessage("\n" +
+                            "请你务必审慎，充分理解“服务协议”和“隐私政策”各条款，包括但不限于：为了向你提供即时通讯、内容分享等服务，我们需要手机你的" +
+                            "设备信息、操作日志等个人信息。你可以在“我的”中查看相关条款说明" +
+                            "你可阅读" +
+                            "《服务协议和隐私政策》了解详情。如你同意，请点击\"同意\"开始接受我们的服务")
+                    .setNegativeButton("暂不使用") { p0, p1 -> }
+                    .setPositiveButton("同意") { p0, p1 ->
+                        mine_check.isChecked = true
+                    }.create().show();
         })
     }
 }
--- business/play_mine/src/main/java/com/will/play/mine/ui/viewmodel/MineLoginViewModel.kt
+++ business/play_mine/src/main/java/com/will/play/mine/ui/viewmodel/MineLoginViewModel.kt
@@ -1,8 +1,10 @@
 package com.will.play.mine.ui.viewmodel
 
 import android.app.Application
+import android.os.Bundle
 import android.text.TextUtils
 import android.view.View
+import androidx.databinding.ObservableBoolean
 import androidx.databinding.ObservableField
 import androidx.databinding.ObservableInt
 import androidx.lifecycle.viewModelScope
@@ -10,13 +12,17 @@ import com.alibaba.android.arouter.launcher.ARouter
 import com.will.habit.base.BaseViewModel
 import com.will.habit.binding.command.BindingAction
 import com.will.habit.binding.command.BindingCommand
+import com.will.habit.binding.command.BindingConsumer
 import com.will.habit.bus.event.SingleLiveEvent
 import com.will.habit.constant.ConstantConfig
 import com.will.habit.extection.launch
 import com.will.habit.extection.toJson
+import com.will.habit.http.RetrofitClient
 import com.will.habit.utils.SPUtils
 import com.will.habit.utils.StringUtils
 import com.will.habit.utils.ToastUtils
+import com.will.play.base.web.WebViewActivity
+import com.will.play.base.web.WebViewPath
 import com.will.play.mine.R
 import com.will.play.mine.BR
 import com.will.play.mine.repository.MineLoginRepository
@@ -37,10 +43,11 @@ class MineLoginViewModel(application: Application) : BaseViewModel<MineLoginRepo
     val userPassword = ObservableField("")
     val verifyBtnVisible = ObservableInt(View.GONE)
 
+    val privateCheck = ObservableBoolean(false)
     val verifyHint = ObservableField(StringUtils.getStringResource(R.string.mine_douyin_verify_hint_password))
 
     val verifyText = ObservableField(StringUtils.getStringResource(R.string.mine_douyin_verify_title_phone))
-
+    val showPhoneCallDialog = SingleLiveEvent<Void>()
     val douyinLogin = SingleLiveEvent<Void>()
     val verifyTitleCLick = BindingCommand<Any>(object : BindingAction {
         override fun call() {
@@ -58,25 +65,29 @@ class MineLoginViewModel(application: Application) : BaseViewModel<MineLoginRepo
 
     val onLoginClick = BindingCommand<Any>(object : BindingAction {
         override fun call() {
-            if (!TextUtils.isEmpty(userAccount.get())||!TextUtils.isEmpty(userPassword.get())) {
-                launch({
-                    showDialog()
-                    if (verifyBtnVisible.get() == View.VISIBLE) {
-                        val data = model.checkVerifyCode(userAccount.get(), userPassword.get())
-                        SPUtils.instance.put(ConstantConfig.TOKEN, data.Token)
-                    } else {
-                        val data = model.login(userAccount.get(), userPassword.get())
-                        SPUtils.instance.put(ConstantConfig.USER_INFO, data.userInfo.toJson())
-                        SPUtils.instance.put(ConstantConfig.TOKEN, data.Token)
-                    }
-                    dismissDialog()
-                    finish()
-                    ARouter.getInstance().build("/app/home").navigation()
-                },{
-                    dismissDialog()
-                })
-            }else{
-                ToastUtils.showShort("用户名或密码不能为空")
+            if(!privateCheck.get()){
+                showPhoneCallDialog.call()
+            }else {
+                if (!TextUtils.isEmpty(userAccount.get()) || !TextUtils.isEmpty(userPassword.get())) {
+                    launch({
+                        showDialog()
+                        if (verifyBtnVisible.get() == View.VISIBLE) {
+                            val data = model.checkVerifyCode(userAccount.get(), userPassword.get())
+                            SPUtils.instance.put(ConstantConfig.TOKEN, data.Token)
+                        } else {
+                            val data = model.login(userAccount.get(), userPassword.get())
+                            SPUtils.instance.put(ConstantConfig.USER_INFO, data.userInfo.toJson())
+                            SPUtils.instance.put(ConstantConfig.TOKEN, data.Token)
+                        }
+                        dismissDialog()
+                        finish()
+                        ARouter.getInstance().build("/app/home").navigation()
+                    }, {
+                        dismissDialog()
+                    })
+                } else {
+                    ToastUtils.showShort("用户名或密码不能为空")
+                }
             }
         }
     })
@@ -85,6 +96,21 @@ class MineLoginViewModel(application: Application) : BaseViewModel<MineLoginRepo
     val onWechatClick = BindingCommand<Any>(object : BindingAction {
         override fun call() {
 
+        }
+    })
+
+    val onCheckChange = BindingCommand<Boolean>(object :BindingConsumer<Boolean>{
+        override fun call(t: Boolean) {
+
+        }
+
+    })
+    val onPrivateClick = BindingCommand<Any>(object : BindingAction {
+        override fun call() {
+            val bundle = Bundle().apply {
+                putString(WebViewPath.URL,"http://test.weizhiyx.com/api.php/Webpage/privacy")
+            }
+            startActivity(WebViewActivity::class.java,bundle)
         }
     })
 
