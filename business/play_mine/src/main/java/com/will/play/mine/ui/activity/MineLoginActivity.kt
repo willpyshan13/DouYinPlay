298962edd3147a200f656f9d11143c6b070a658b
will <545512533@qq.com>
2020-10-12 14:25:19 +0800

    文案修改

--- business/play_mine/src/main/java/com/will/play/mine/ui/activity/MineLoginActivity.kt
+++ business/play_mine/src/main/java/com/will/play/mine/ui/activity/MineLoginActivity.kt
@@ -51,7 +51,7 @@ class MineLoginActivity : BaseActivity<MineActivityAddressBinding, MineLoginView
                             "请你务必审慎，充分理解“服务协议”和“隐私政策”各条款，包括但不限于：为了向你提供即时通讯、内容分享等服务，我们需要手机你的" +
                             "设备信息、操作日志等个人信息。你可以在“我的”中查看相关条款说明" +
                             "你可阅读" +
-                            "《服务协议和隐私政策》了解详情。如你同意，请点击\"同意\"开始接受我们的服务")
+                            "《用户服务协议》和《隐私协议》了解详情。如你同意，请点击\"同意\"开始接受我们的服务")
                     .setNegativeButton("暂不使用") { p0, p1 -> }
                     .setPositiveButton("同意") { p0, p1 ->
                         mine_check.isChecked = true
